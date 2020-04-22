7d7114b4a3c8e5a758fee902f8a304641b75a40c
Siddique Data Engineer <dba.siddique@gmail.com>
2020-04-22 13:37:00 +0500

    Implement data processing pipeline

---
+++ src/main.py
@@ -0,0 +1,16 @@
+"""
+52 Spark Ml Feature Engineering - Main Implementation
+"""
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    logger.info("Starting 52-spark-ml-feature-engineering")
+    # Implementation here
+    pass
+
+if __name__ == "__main__":
+    main()
